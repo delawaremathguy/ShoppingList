4a5dd8334beba7918b5b41e51300efab636b64e7
Jerry Keough <delawaremathguy@icloud.com>
2020-04-22 20:17:53 -0400

    Initial Commit

---
+++ ShoppingList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoppingList
+//
+//  Created by Jerry on 4/22/20.
+//  Copyright © 2020 Jerry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
