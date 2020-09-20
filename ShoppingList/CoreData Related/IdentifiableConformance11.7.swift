651399475a3aecc03e5c2822da94d969c9a5104c
Jerry Keough <delawaremathguy@icloud.com>
2020-09-20 11:15:30 -0400

    revert to Xcode 11.7 status.

---
+++ ShoppingList/CoreData Related/IdentifiableConformance11.7.swift
@@ -0,0 +1,28 @@
+//
+//  IdentifiableConformance11.7.swift
+//  ShoppingList
+//
+//  Created by Jerry on 9/20/20.
+//  Copyright © 2020 Jerry. All rights reserved.
+//
+
+import Foundation
+
+// Identifiable Conformance for XCode 11
+
+// the lines below must be added if you are using XCode 11 so that ShoppingItem and Location
+// conform to the Identifiable protocol.  there is no code here: each has an id
+// attribute (a UUID) already defined in the Core Data model; we just need to make
+// XCode aware of that.  in XCode 12, the files ShoppingItem+CoreDataProperties and
+// Location+CoreDataProperties that are automatically generated include these
+// conformances, probably because the id field is recognized by name and its
+// type (UUID) is Hashable.  that results in duplicate conformance errors if
+// this file is included.  to compile under XCode 12, remove this file.
+
+extension ShoppingItem: Identifiable {
+	/* id is already defined in the core data model */
+}
+
+extension Location: Identifiable {
+	/* id is already defined in the core data model */
+}
--- ShoppingList/General Purpose Views/SLSectionHeaderView.swift
+++ ShoppingList/General Purpose Views/SLSectionHeaderView.swift
@@ -12,26 +12,31 @@ import SwiftUI
 // there's certainly a better way to do it using a customized ListStyle.
 // but that's a matter for another day.  besides, the View we provide
 // here does not really own the space in which it appears.
+
+// XCODE 12 NOTE.  uncomment-out the commented lines below (this code will
+// not compile under XCode 11) to get the iOS 13 appearance of section headers.
+
 struct SLSectionHeaderView: View {
 	
 	var title: String
 	
 	var body: some View {
 		GeometryReader { geo in
-			if #available(iOS 14, *) {
-				// default section titles in iOS 14 are upper-case and left-justified
-				// so this uses case as intended and centers the title, as in iOS 13
-				Text(title)
+//			if #available(iOS 14, *) {
+//				// default section titles in iOS 14 are upper-case and left-justified
+//				// so this uses case as intended and centers the title, as in iOS 13
+//				Text(title)
+//					.font(.body)
+//					.foregroundColor(.black)
+//					.textCase(.none) // an iOS 14 modifier
+//					.position(x: geo.size.width/2, y: geo.size.height/2)
+//
+//			} else {
+			Text(self.title)
 					.font(.body)
 					.foregroundColor(.black)
-					.textCase(.none) // an iOS 14 modifier
 					.position(x: geo.size.width/2, y: geo.size.height/2)
-				
-			} else {
-				Text(title)
-					.font(.body)
-					.foregroundColor(.black)
-			}
+//			}
 		}
 	}
 	
